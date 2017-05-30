f25118b7591900fc740026be8b2ae916b3c20705
Manan Doshi <mdoshi@login1.maverick.tacc.utexas.edu>
2017-05-30 15:26:24 -0500

    regularisation

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ alpha           = parser.parse_args().alpha
 learning_rate   = parser.parse_args().learning_rate
 training_epochs = parser.parse_args().training_epochs
 save_to         = "result" + parser.parse_args().res_n
-
+regularizer = tf.contrib.layers.l2_regularizer(alpha)
 batch_size = parser.parse_args().batch_size
 data = DataWrapper(filename=parser.parse_args().inp_filename, batch_size=batch_size)
 
@@ -41,21 +41,21 @@ X = tf.placeholder("float32", [None, 64, 1])
 Y = tf.placeholder("float32", [None, 64, 1])
 conv1 = []; pool1 = []; conv2 = []; pool2 = []; fc1=[];
 for i, Dim in enumerate([X,Y]):
-    conv1.append(tf.layers.conv1d(Dim, p, w, data_format="channels_last"))                                 #(?,4,60 (+4))
+    conv1.append(tf.layers.conv1d(Dim, p, w, kernel_regularizer=regularizer, data_format="channels_last"))                                 #(?,4,60 (+4))
     pool1.append(tf.layers.average_pooling1d(conv1[i],2,2, data_format="channels_last"))                   #(?,4,30 (+2))
-    conv2.append(tf.layers.conv1d(pool1[i],int(p*(p-1)/2),w, data_format="channels_last"))                 #(?,32,6)
+    conv2.append(tf.layers.conv1d(pool1[i],int(p*(p-1)/2),w, kernel_regularizer=regularizer, data_format="channels_last"))                 #(?,32,6)
     pool2.append(tf.layers.average_pooling1d(conv2[i],4,4, data_format="channels_last"))                   #(?,8,6)
     #for channel in X[]:
-    fc1.append(tf.layers.dense(tf.transpose(pool2[i], perm=[0,2,1]),8))                                    #(?,6,8)
+    fc1.append(tf.layers.dense(tf.transpose(pool2[i], perm=[0,2,1]),8, kernel_regularizer=regularizer))                                    #(?,6,8)
     #fc1.unroll()
 
 conc        = tf.concat([fc1[0],fc1[1]], -1)
 reshaped    = tf.reshape( conc , [batch_size,p*(p-1)*8]) 
-enc = tf.layers.dense( reshaped,20 )
+enc = tf.layers.dense( reshaped, 20, kernel_regularizer=regularizer)
 
 #DECODER
-fc_deconv = tf.split(tf.layers.dense(enc, int(16*p*(p-1)/2)), num_or_size_splits=2, axis=1)
-fc_deconv2 = [tf.layers.dense(i, int(8*p*(p-1)/2)) for i in fc_deconv]
+fc_deconv = tf.split(tf.layers.dense(enc, int(16*p*(p-1)/2), kernel_regularizer=regularizer), num_or_size_splits=2, axis=1)
+fc_deconv2 = [tf.layers.dense(i, int(8*p*(p-1)/2), kernel_regularizer=regularizer) for i in fc_deconv]
 
 enc_2 = [tf.reshape(i, [batch_size, 8, 1, int(p*(p-1)/2)]) for i in fc_deconv2] # height 8, width 1, channel p*(p-1)/2. NHWC
 
@@ -65,14 +65,18 @@ filter_deconv2 = [0, 0]
 filter_deconv1 = [0, 0]
 
 for i in xrange(2):
+
     filter_deconv1[i] = tf.Variable(tf.random_normal([w, 1, p, int(p*(p-1)/2)], stddev=0.5))
     deconv1[i] = tf.nn.conv2d_transpose(enc_2[i], filter_deconv1[i], output_shape=[batch_size, 32, 1, p], strides=[1, 4, 4, 1])
-    filter_deconv2[i] = tf.Variable(tf.random_normal([w, 1, 1, p], stddev=0.5))
+    filter_deconv2[i] = tf.Variable(tf.random_normal([w, 1, 1, p] , stddev=0.5))
     deconv2[i] = tf.nn.conv2d_transpose(deconv1[i], filter_deconv2[i], output_shape=[batch_size, 64, 1, 1], strides=[1, 2, 2, 1])
+
+    tf.add_to_collection(tf.GraphKeys.REGULARIZATION_LOSSES,regularizer(filter_deconv1[i]))
+    tf.add_to_collection(tf.GraphKeys.REGULARIZATION_LOSSES,regularizer(filter_deconv2[i]))
 
 decX = tf.reshape(deconv2[0], [batch_size, 64])
 decY = tf.reshape(deconv2[1], [batch_size, 64])
-
+reg_term = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
 
 #y_true = tf.concat(X,Y)
 #y_pred = tf.concat(decX, decY)
